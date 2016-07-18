6d2646ae88c61f96e7a41c27287b66a95507397e
CMUFeiyue2016 <drawmouse@gmail.com>
2016-07-18 11:58:14 -0400

    trying to fix chassis

--- ExampleRobot/src/org/usfirst/frc/team3504/robot/RobotMap.java
+++ ExampleRobot/src/org/usfirst/frc/team3504/robot/RobotMap.java
@@ -1,27 +1,39 @@
 package org.usfirst.frc.team3504.robot;
-/**
- * The RobotMap is a mapping from the ports sensors and actuators are wired into
- * to a variable name. This provides flexibility changing wiring, makes checking
- * the wiring easier and significantly reduces the number of magic numbers
- * floating around.
- */
+
 public class RobotMap {
-	public static final int DRIVE_MASTER = 1;
-	public static final int DRIVE_SLAVE = 3;
+
+	//TODO: Fix ports for motors
+	public static final int MASTER_LEFT = 1;
+	public static final int SLAVE_LEFT = 1;
+	
+	public static final int MASTER_RIGHT= 3;
+	public static final int SLAVE_RIGHT= 3;
 	
 	public static final int JOYSTICK_PORT = 0;
 	
+	// List of cameras on the robot
 	public static final String[] CAMERA_NAMES = {"cam0"};
+	
 	// Encoder-to-distance constants
-		// How many ticks are there on the encoder wheel?
-		private static final double pulsePerRevolution = 360;
-		// How far to we travel when the encoder turns one full revolution?
-		// Gear ratio is turns of the wheel per turns of the encoder
-		private static final double distancePerRevolutionHighGear = 8.0/*wheel size*/ * Math.PI * (1/27.21)/*gear ratio*/; //(9.07)
-		private static final double distancePerRevolutionLowGear = 8.0/*wheel size*/ * Math.PI * (1/28.33)/*gear ratio*/; //28.33/15.9 (33.33) 
-	    // Given our set of wheels and gear box, how many inches do we travel per pulse?
-		public static final double DISTANCE_PER_PULSE_HIGH_GEAR = distancePerRevolutionHighGear / pulsePerRevolution;
-		public static final double DISTANCE_PER_PULSE_LOW_GEAR = distancePerRevolutionLowGear / pulsePerRevolution;
+	// How many ticks are there on the encoder wheel?
+	private static final double pulsePerRevolution = 360;
+
+	// How far to we travel when the encoder turns one full revolution?
+	// Gear ratio is turns of the wheel per turns of the encoder
+	private static final double wheelSize = 8.0;
+	private static final double highGearRatio = (1/27.21);
+	private static final double lowGearRatio = (1/28.33);
+	
+	private static final double distPerRevolutionHighGear = 
+			wheelSize * Math.PI * highGearRatio; //(9.07)
+	private static final double distPerRevolutionLowGear = 
+			wheelSize * Math.PI * lowGearRatio; //28.33/15.9 (33.33) 
+    
+	// Given our set of wheels and gear box, how many inches do we travel per pulse?
+	public static final double DIST_PER_PULSE_HIGH_GEAR = 
+			distPerRevolutionHighGear / pulsePerRevolution;
+	public static final double DIST_PER_PULSE_LOW_GEAR = 
+			distPerRevolutionLowGear / pulsePerRevolution;
 
 
 }
