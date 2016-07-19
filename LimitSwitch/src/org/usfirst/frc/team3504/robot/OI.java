fc726f44a60ed1c013517297ce1c0869af8fbf3c
CMUFeiyue2016 <drawmouse@gmail.com>
2016-07-19 10:33:21 -0400

    added limit switch project

---
+++ LimitSwitch/src/org/usfirst/frc/team3504/robot/OI.java
@@ -0,0 +1,19 @@
+package org.usfirst.frc.team3504.robot;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.buttons.Button;
+import edu.wpi.first.wpilibj.buttons.JoystickButton;
+import org.usfirst.frc.team3504.robot.commands.DriveUntilLimitSwitchIsPressed;
+
+public class OI {
+	
+	Joystick Joy = new Joystick(1); 
+	Button button3 = new JoystickButton(Joy, 3); //drive forward after button release until limit bumped 
+//	Button button4 = new JoystickButton(Joy, 4); //after button, drive forward while limit held down
+	
+	public OI () 
+	{
+		button3.whenReleased(new DriveUntilLimitSwitchIsPressed());
+	//	button4.whenReleased(new DriveWhileLimitSwitchIsPressed());
+	}
+}
+
